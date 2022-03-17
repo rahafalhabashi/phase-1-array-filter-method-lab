21c137872dbea561f8dd23993ea679f0dacdc1f8
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-17 13:06:29 -0400

    commit tree c3ab0a79ebcd63db7e74d762f5dbbebce1b9d1b6

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(arrNames, string) {
+    return arrNames.filter(elementInArray => elementInArray.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(arrNames, string) {
+    return arrNames.filter(driverName => driverName.charAt(0) === string.charAt(0))
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(driver => driver.name === str)
+}
+
+console.log(matchName);
+
+
+
+
+
+
+
+
+
+
+
+
+// const findMatching = drivers.filter() 
+
+//(array, check against array)
+// 
